f5d08933c78be10ec54776682cc15ccb3155aa06
iddar <iddar@dbug.mx>
2019-08-25 08:47:59 -0500

    extrac audio

--- frameFromVideo.js
+++ frameFromVideo.js
@@ -1,6 +1,8 @@
 const {Storage} = require('@google-cloud/storage');
 const extractFrames = require('ffmpeg-extract-frames')
 const shortid = require('shortid')
+const extractAudio = require('ffmpeg-extract-audio')
+
 
 const downloadFile = require('./downloadFile')
 const path = './.tmp'
@@ -11,22 +13,37 @@ const storage = new Storage({
 
 const bucketName = 'credibot'
 
-module.exports = async function getFrame(url) {
-  let filePath = await downloadFile(url, path, 'mp4')
-  let framePath = `${path}/${shortid.generate()}.jpg`
-  
-  await extractFrames({
-    input: filePath,
-    output: framePath,
-    offsets: [1000]
-  })
-
-  await storage.bucket(bucketName).upload(framePath, {
+function saveToCloud (filePath) {
+  return storage.bucket(bucketName)
+  .upload(filePath, {
     gzip: true,
     metadata: {
       cacheControl: 'public, max-age=31536000',
     },
-  });
+  })
+}
+
+module.exports = async function getFrame(url) {
+  let videoPath = await downloadFile(url, path, 'mp4')
+  let framePath = `${path}/${shortid.generate()}.jpg`
+  let audioPath = `${path}/${shortid.generate()}.mp3`
+  
+  await extractFrames({
+    input: videoPath,
+    output: framePath,
+    offsets: [1000]
+  })
+
+  await extractAudio({
+    input: videoPath,
+    output: audioPath
+  })
+
+
+  await Promise.all([
+    saveToCloud(framePath),
+    saveToCloud(audioPath)
+  ])
 
   return framePath
 }

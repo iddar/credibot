9dce6e3d984dfcccf5735fd7ac308c8f5dd81ca7
iddar <iddar@dbug.mx>
2019-08-25 08:34:34 -0500

    g storage

--- frameFromVideo.js
+++ frameFromVideo.js
@@ -1,8 +1,15 @@
+const {Storage} = require('@google-cloud/storage');
 const extractFrames = require('ffmpeg-extract-frames')
 const shortid = require('shortid')
 
 const downloadFile = require('./downloadFile')
 const path = './.tmp'
+
+const storage = new Storage({
+  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+})
+
+const bucketName = 'credibot'
 
 module.exports = async function getFrame(url) {
   let filePath = await downloadFile(url, path, 'mp4')
@@ -13,6 +20,13 @@ module.exports = async function getFrame(url) {
     output: framePath,
     offsets: [1000]
   })
+
+  await storage.bucket(bucketName).upload(framePath, {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
+  });
 
   return framePath
 }
